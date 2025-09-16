ee2569842d263e8d0dbf8dcec40386b7665546d1
WndrB <225472602+WndrB@users.noreply.github.com>
2025-09-15 18:41:16 -0600

    Write the logic that determines the computer's choice
    Add a getComputerChoice() function that randomly returns rock, paper, or scissors.
    
    Then, test the function to see if it returns the expected result.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Let's play!")
+
+function getComputerChoice() {
+    let choice;
+    const random_number = Math.random() * 3;
+    if (random_number < 1) {
+       choice = "rock"
+    } else if (random_number < 2) {
+       choice = "paper"
+    } else {
+      choice = "scissors"
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
