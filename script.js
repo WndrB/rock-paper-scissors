162e01dce2a7f5e4e877575e245e0663066ef031
WndrB <225472602+WndrB@users.noreply.github.com>
2025-09-16 14:42:46 -0600

    Write the logic to play the entire game.
    Create a new function named playGame().
    
    Move the playRound() function and score variables inside the playGame() function.
    
    Call playRound() function five times.
    
    Modify the getHumanChoice() function so that it can accept a parameter instead of a fixed string.
    
    Create a new function, showFinalScore(), to display the score after the five
    rounds have been played.
    
    Add some comments.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
 
+
+// Make a random computer choice
 function getComputerChoice() {
     let choice;
     const random_number = Math.random() * 3;
@@ -14,32 +14,70 @@ function getComputerChoice() {
     return choice;
 }
 
-function getHumanChoice() {
-    choice = prompt('Write "rock," "paper," or "scissors."', "");
+// Get the user's choice.
+function getHumanChoice(message) {
+    choice = prompt(message, "");
     return choice.toLowerCase();
 }
 
-function playRound(computerChoice, humanChoice) {
-    choices = computerChoice + humanChoice;
-    let result;
-    if (choices === "rockscissors" || choices === "paperrock" || choices === "scissorspaper") {
-        result = `You lose! ${computerChoice} beats ${humanChoice}.`;
-        computerScore++;
-    } else if (choices === "scissorsrock" || choices === "rockpaper" || choices === "paperscissors") {
-        result = `You win! ${humanChoice} beats ${computerChoice}.`;
-        humanScore++;
-    } else {
+// Play five rounds
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    // Play only one round
+    function playRound(computerChoice, humanChoice) {
+        choices = computerChoice + humanChoice;
+        let result;
+        if (choices === "rockscissors" || choices === "paperrock" || choices === "scissorspaper") {
+            result = `You lose! ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
+        } else if (choices === "scissorsrock" || choices === "rockpaper" || choices === "paperscissors") {
+            result = `You win! ${humanChoice} beats ${computerChoice}.`;
+            humanScore++;
+        } else {
         result = `It's a tie.`;
+        }
+        return result;
     }
-    return result;
+    
+    // Show the final score after the five rounds
+    function showFinalScore(computerScore, humanScore) {
+        console.log("#####")
+        console.log(`The final score is: Computer = ${computerScore} pts. You = ${humanScore} pts.`);
+    }
+    
+    // round 1
+    console.log("Round One!")
+    humanSelection = getHumanChoice("Round one! Type rock, paper, or scissors.");
+    computerSelection = getComputerChoice();
+    console.log(playRound(computerSelection, humanSelection));
+    
+    // round 2
+    console.log("Round Two!")
+    humanSelection = getHumanChoice("Round Two! Type rock, paper, or scissors.");
+    computerSelection = getComputerChoice();
+    console.log(playRound(computerSelection, humanSelection));
+    
+    // round 3
+    console.log("Round Three!")
+    humanSelection = getHumanChoice("Round Three! Type rock, paper, or scissors.");
+    computerSelection = getComputerChoice();
+    console.log(playRound(computerSelection, humanSelection));
+    
+    // round 4
+    console.log("Round Four!")
+    humanSelection = getHumanChoice("Round Four! Type rock, paper, or scissors.");
+    computerSelection = getComputerChoice();
+    console.log(playRound(computerSelection, humanSelection));
+    
+    // round 5
+    console.log("Last Round!")
+    humanSelection = getHumanChoice("Last Round! Type rock, paper, or scissors.");
+    computerSelection = getComputerChoice();
+    console.log(playRound(computerSelection, humanSelection));
+    
+    showFinalScore(computerScore, humanScore);
 }
 
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-
-console.log(computerSelection)
-console.log(humanSelection)
-
-console.log(playRound(computerSelection, humanSelection));
-console.log(computerScore)
-console.log(humanScore)
+playGame();
